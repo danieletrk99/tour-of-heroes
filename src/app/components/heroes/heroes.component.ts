14d3a378367cb88411a5eb3e95c248b512ef0b4e
HyperDogeExe <dnltrct@gmail.com>
2024-06-03 17:12:59 +0200

    test commit

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Hero } from '../../interfaces/hero';
+import { HEROES } from '../../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+
+  heroes = HEROES;
+}
