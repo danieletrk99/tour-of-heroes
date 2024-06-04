8b309798868a7a65feee2986858f27679d495380
luizfdr <luizefedrigorossi@gmail.com>
2024-06-04 11:21:32 +0200

    added messages service

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../interfaces/hero';
 import { HEROES } from '../../mock-heroes';
 import { HeroServiceService } from '../../services/hero-service.service';
+import { MessageService } from '../../services/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,16 +16,21 @@ export class HeroesComponent {
     this.getHeroes();
   }
 
-  constructor(private heroService : HeroServiceService){}
+  constructor(
+    private heroService : HeroServiceService,
+    private messageService : MessageService
+  ){}
 
   getHeroes() : void{
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
   }
 
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   heroes : Hero[] = [];
