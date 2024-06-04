2cac9df8d299c7ab966987b969995e4577723021
HyperDogeExe <dnltrct@gmail.com>
2024-06-04 16:27:30 +0200

    added various http requests and refactored some methods

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Hero } from '../interfaces/hero';
-import { HEROES } from '../mock-heroes';
-import { Observable, of } from 'rxjs';
+import { Observable, catchError, of, tap } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +10,68 @@ import { MessageService } from './message.service';
 
 export class HeroService {
 
-  constructor(private messageService : MessageService) { }
+  constructor(
+    private messageService : MessageService,
+    private http: HttpClient
+  ) { }
+
+  private log (message: string){
+    this.messageService.add(`Hero service: ${message}`)
+  }
+  private heroesUrl = 'api/heroes';
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+          catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   getHeroById(id: number): Observable<Hero>{
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`Hero Service: fetched hero id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  };
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-type': 'application/json'})
+  };
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>("updateHero"))
+    );
   }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero : Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  deleteHero(id: number): Observable<Hero>{
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T){
+    return(error: any): Observable<T> => {
+      // send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+    }
+  };
 }
