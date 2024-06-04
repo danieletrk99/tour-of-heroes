c73ea371b34b4601c7b33aa3b9b52566109aa29c
StefanoDFranceschi <stedefranceschi06@gmail.com>
2024-06-04 10:28:18 +0200

    Hero Services completed

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
 import { Hero } from '../../interfaces/hero';
 import { HEROES } from '../../mock-heroes';
+import { HeroService } from '../../services/hero.service';
+import { OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
+
 export class HeroesComponent {
+  ngOnInit() :void{
+    this.getHeroes();
+  }
+  constructor(private heroService:HeroService){}
+  getHeroes():void{
+    this.heroes = this.heroService.getHeroes();
+  }    
+
+
+
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
 
-  heroes = HEROES;
+  heroes : Hero[] = []
 }
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import { Hero } from '../interfaces/hero';
+import { HEROES } from '../mock-heroes';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
+  getHeroes():Hero[]{
+    return HEROES;
+  }
   constructor() { }
 }
