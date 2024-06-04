934569f11b16f7824d0d04fa2f9f21535d8e20e9
luizfdr <luizefedrigorossi@gmail.com>
2024-06-04 10:28:04 +0200

    services setup for HEROES

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../interfaces/hero';
 import { HEROES } from '../../mock-heroes';
+import { HeroServiceService } from '../../services/hero-service.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
+
+
 export class HeroesComponent {
+  ngOnInit() : void {
+    this.getHeroes();
+  }
+
+  constructor(private heroService : HeroServiceService){}
+
+  getHeroes() : void{
+    this.heroes = this.heroService.getHeroes();
+  }
+
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
 
-  heroes = HEROES;
+  heroes : Hero[] = [];
 }
